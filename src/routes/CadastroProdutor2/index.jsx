b8f1ebdb59c5fe0176e45868cf6d1deabb80e9d7
AnaClaraSM <itsanaclarasm.dev@gmail.com>
2024-11-11 16:14:00 -0300

    Ben - CadProd2

---
+++ src/routes/CadastroProdutor2/index.jsx
@@ -0,0 +1,65 @@
+// CadastroProdutor2.js
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function CadastroProdutor2() {
+  const [formData, setFormData] = useState({
+    nomeFantasia: '',
+    tamanhoPropriedade: '',
+    cep: '',
+    estado: '',
+    cnpj: '',
+    tipoProducao: '',
+    cidade: '',
+    pais: '',
+  });
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (Object.values(formData).some((field) => field.trim() === '')) {
+      alert('Por favor, preencha todos os campos.');
+      return;
+    }
+    navigate('/compras'); // Navega para a página de compras
+  };
+
+  return (
+    <div className="container d-flex justify-content-center align-items-center">
+      <div className="card px-5">
+        <h2 className="text-center container-login">CADASTRO</h2>
+        <p className="text-center containe-bg">PRODUTOR</p>
+        <form onSubmit={handleSubmit} className="form-group">
+          <div className="row pt-3">
+            {/* Nome Fantasia */}
+            <div className="col-3">
+              <label className="form-label mt-4 fw-semibold">NOME FANTASIA</label>
+              <input type="text" className="form-control input-color p-3" name="nomeFantasia" onChange={handleChange} />
+            </div>
+            {/* CEP */}
+            <div className="col-3">
+              <label className="form-label mt-4 fw-semibold">CEP</label>
+              <input type="text" className="form-control input-color p-3" name="cep" onChange={handleChange} />
+            </div>
+            {/* Continue com os outros campos */}
+          </div>
+          <div className="row pt-5">
+            <button type="submit" className="form-control btn btn-info mt-3" style={{ width: 250, height: 50, borderRadius: 40 }}>
+              FINALIZAR
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default CadastroProdutor2;
