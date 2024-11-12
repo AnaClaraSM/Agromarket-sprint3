6ec99529903c1923de86d8e99a608cde7ae6083f
AnaClaraSM <itsanaclarasm.dev@gmail.com>
2024-11-11 23:00:01 -0300

    classNames

--- src/components/HomeEcossistema.jsx
+++ src/components/HomeEcossistema.jsx
@@ -3,28 +3,28 @@ import { StyledHomeEcossistema } from '../styled';
 
 const HomeEcossistema = () => (
     <StyledHomeEcossistema>
-        <section class="ecossistema" id="ecossistema">
+        <section className="ecossistema" id="ecossistema">
             {/* <!-- Cabeçalho --> */}
-            <div class="section-header">
+            <div className="section-header">
                 <h2>Nosso Ecossistema</h2>
                 <p>Nossa plataforma integra três diferentes nichos para formar o ciclo completo da cadeia produtiva.</p>
             </div>
             {/* <!-- Cards --> */}
-            <div class="container-ecossistema">
+            <div className="container-ecossistema">
                 {/* <!-- Produtores --> */}
-                <div class="card-ecossistema">
+                <div className="card-ecossistema">
                     <h3>Produtores</h3>
                     <img src="./assets/images/ecossistema1.png" alt="" />
                     <p>Os pequenos produtores compõem o primeiro nicho do nosso ecossistema, fornecendo alimentos naturais e de qualidade diretamente para os consumidores.</p>
                 </div>
                 {/* <!-- Consumidores --> */}
-                <div class="card-ecossistema">
+                <div className="card-ecossistema">
                     <h3>Consumidores</h3>
                     <img src="./assets/images/ecossistema2.png" alt="" />
                     <p>Os consumidores são o segundo nicho, que movimenta a cadeia comprando produtos, trazendo resíduos, economizando com descontos e participando ativamente desse sistema.</p>
                 </div>
                 {/* <!-- Coletores --> */}
-                <div class="card-ecossistema">
+                <div className="card-ecossistema">
                     <h3>Coletores</h3>
                     <img src="./assets/images/ecossistema3.png" alt="" />
                     <p>Os coletores integram o terceiro nicho, sendo nossos parceiros diretos e os responsáveis por garantir o processamento correto de todos os resíduos cadastrados.</p>
